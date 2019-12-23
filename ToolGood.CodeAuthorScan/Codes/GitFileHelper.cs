7406ef093ed01a595a7e4f751d9a229efe2ba10a
lzj <toolgood@qq.com>
2019-12-23 14:31:49 +0800

    使用 NGIT

--- ToolGood.CodeAuthorScan/Codes/GitFileHelper.cs
+++ ToolGood.CodeAuthorScan/Codes/GitFileHelper.cs
@@ -1,7 +1,9 @@
 ﻿using LibGit2Sharp;
+using NGit.Api;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using ToolGood.CodeAuthorScan.Datas;
 namespace ToolGood.CodeAuthorScan.Codes
@@ -21,72 +23,81 @@ namespace ToolGood.CodeAuthorScan.Codes
             return GetFolderPath(Path.GetDirectoryName(filePath));
         }
 
-        public static List<GitFileInfo> GetFileInfo(string repositoryFolder, string file)
-        {
-            List<GitFileInfo> infos = new List<GitFileInfo>();
-            var repository = new Repository(repositoryFolder);
-            if (repositoryFolder.StartsWith("\\") == false) {
-                repositoryFolder = repositoryFolder + "\\";
-            }
-            var rFile = file.Replace(repositoryFolder, "").Replace("\\", "/");
-            try {
-                
-                var t = repository.Blame(rFile,new BlameOptions { StartingAt = repository.Head });
-                //var t2 = repository.Blame(rFile);
-                Console.WriteLine($"获取文件上传信息：{file} ");
 
-                foreach (var item in t) {
-                    GitFileInfo info = new GitFileInfo() {
-                        File = file,
-                        Line = item.FinalStartLineNumber,
-                        Author = item.FinalCommit.Author.Name,
-                        CommitTime = item.FinalCommit.Author.When.DateTime
-                    };
-                    infos.Add(info);
-                }
-            } catch (Exception) { }
-            return infos;
-        }
-        public static List<GitFileInfo> GetFileInfo(string repositoryFolder, string file, int startLine, int endLine)
-        {
-            List<GitFileInfo> infos = new List<GitFileInfo>();
-            var repository = new Repository(repositoryFolder);
-            if (repositoryFolder.StartsWith("\\") == false) {
-                repositoryFolder = repositoryFolder + "\\";
-            }
-            var rFile = file.Replace(repositoryFolder, "").Replace("\\", "/");
-            try {
-                var t = repository.Blame(rFile, new BlameOptions() { MinLine = startLine + 1, MaxLine = endLine + 1, Strategy = BlameStrategy.Default });
-                Console.WriteLine($"获取文件上传信息：{file} ");
 
-                foreach (var item in t) {
-                    GitFileInfo info = new GitFileInfo() {
-                        File = file,
-                        Line = item.FinalStartLineNumber,
-                        Author = item.FinalCommit.Author.Name,
-                        CommitTime = item.FinalCommit.Author.When.DateTime
-                    };
-                    infos.Add(info);
-                }
-            } catch (Exception) { }
-            return infos;
+        private Dictionary<string, Git> gitDict = new Dictionary<string, Git>();
 
 
 
-            //List<GitFileInfo> infos = new List<GitFileInfo>();
-            //var repository = new Repository(RepositoryFolder);
-            //var t = repository.Blame(file, new BlameOptions() { MinLine = startLine + 1, MaxLine = endLine + 1 });
-            //foreach (var item in t) {
-            //    GitFileInfo info = new GitFileInfo() {
-            //        File = file,
-            //        Line = item.FinalStartLineNumber,
-            //        Author = item.FinalCommit.Author.Name,
-            //        CommitTime = item.FinalCommit.Author.When.DateTime
-            //    };
-            //    infos.Add(info);
-            //}
-            //return infos;
-        }
+        //public static List<GitFileInfo> GetFileInfo(string repositoryFolder, string file)
+        //{
+        //    List<GitFileInfo> infos = new List<GitFileInfo>();
+        //    var git = Git.Init().SetDirectory(repositoryFolder).Call();
+              
+
+        //    if (repositoryFolder.StartsWith("\\") == false) {
+        //        repositoryFolder = repositoryFolder + "\\";
+        //    }
+        //    var rFile = file.Replace(repositoryFolder, "").Replace("\\", "/");
+
+             
+        //    try {
+        //        var t = repository.Blame(rFile,new BlameOptions { StartingAt = repository.Head,MaxLine=0 });
+        //        //var t2 = repository.Blame(rFile);
+        //        Console.WriteLine($"获取文件上传信息：{file} ");
+
+        //        foreach (var item in t) {
+        //            GitFileInfo info = new GitFileInfo() {
+        //                File = file,
+        //                Line = item.FinalStartLineNumber,
+        //                Author = item.FinalCommit.Author.Name,
+        //                CommitTime = item.FinalCommit.Author.When.DateTime
+        //            };
+        //            infos.Add(info);
+        //        }
+        //    } catch (Exception) { }
+        //    return infos;
+        //}
+        //public static List<GitFileInfo> GetFileInfo(string repositoryFolder, string file, int startLine, int endLine)
+        //{
+        //    List<GitFileInfo> infos = new List<GitFileInfo>();
+        //    var repository = new Repository(repositoryFolder);
+        //    if (repositoryFolder.StartsWith("\\") == false) {
+        //        repositoryFolder = repositoryFolder + "\\";
+        //    }
+        //    var rFile = file.Replace(repositoryFolder, "").Replace("\\", "/");
+        //    try {
+        //        var t = repository.Blame(rFile, new BlameOptions() { MinLine = startLine + 1, MaxLine = endLine + 1, Strategy = BlameStrategy.Default });
+        //        Console.WriteLine($"获取文件上传信息：{file} ");
+
+        //        foreach (var item in t) {
+        //            GitFileInfo info = new GitFileInfo() {
+        //                File = file,
+        //                Line = item.FinalStartLineNumber,
+        //                Author = item.FinalCommit.Author.Name,
+        //                CommitTime = item.FinalCommit.Author.When.DateTime
+        //            };
+        //            infos.Add(info);
+        //        }
+        //    } catch (Exception) { }
+        //    return infos;
+
+
+
+        //    //List<GitFileInfo> infos = new List<GitFileInfo>();
+        //    //var repository = new Repository(RepositoryFolder);
+        //    //var t = repository.Blame(file, new BlameOptions() { MinLine = startLine + 1, MaxLine = endLine + 1 });
+        //    //foreach (var item in t) {
+        //    //    GitFileInfo info = new GitFileInfo() {
+        //    //        File = file,
+        //    //        Line = item.FinalStartLineNumber,
+        //    //        Author = item.FinalCommit.Author.Name,
+        //    //        CommitTime = item.FinalCommit.Author.When.DateTime
+        //    //    };
+        //    //    infos.Add(info);
+        //    //}
+        //    //return infos;
+        //}
 
     }
 }
